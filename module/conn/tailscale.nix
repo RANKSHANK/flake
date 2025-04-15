819144283b75db4970647d39ba5443f7e05ff3f3
rankshank <imapsngamer@gmail.com>
2025-04-16 00:23:15 +1000

    tailscale: add firewall rules

--- module/conn/tailscale.nix
+++ module/conn/tailscale.nix
@@ -3,5 +3,13 @@
 lib.mkModule "tailscale" [ "connectivity" ] {
     services.tailscale = {
         enable = true;
+        useRoutingFeatures = "both";
+        openFirewall = true;
+    };
+
+    networking.firewall = {
+        trustedInterfaces = [
+            "tailscale0"
+        ];
     };
 }
--- module/conn/web-services/nginx.nix
+++ module/conn/web-services/nginx.nix
@@ -4,11 +4,19 @@ lib.mkModule "nginx" [ "server" "connectivity" ] {
     services.nginx = {
         enable = true;
         virtualHosts.${config.nginx.base-url} = {
-            locations."/" = {
-                return = "200 <html><body>Dead End</body></html>";
-                extraConfig = ''
-                    default_type text/html;
-                '';
+            locations = {
+                "/" = {
+                    return = "200 <html><body>Dead End</body></html>";
+                    extraConfig = ''
+                        default_type text/html;
+                    '';
+                };
+                "/robots.txt" = {
+                    extraConfig = ''
+                        rewrite ^/(.*)  $1;
+                        return 200 "User-agent: *\nDisallow: /";
+                    '';
+                };
             };
         };
     };
